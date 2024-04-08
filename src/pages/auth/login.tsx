60d7836b69b74f16a134be6bf9eb6c74039571fa
Robi Irhamni <robidream20@gmail.com>
2024-04-08 13:03:51 +0700

    add taillwind css

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,18 +1,10 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import LoginViews from '@/views/Auth/Login/index';
 
 const LoginPage = () => {
-  const { push } = useRouter();
-  const handlerLogin = () => {
-    push("/product");
-  };
+
   return (
     <>
-      <h1>Login Page</h1>
-      <button onClick={handlerLogin}>Login</button>
-      <p>
-        Belum punya akun? registrasi <Link href="/auth/register">disini</Link>
-      </p>
+      <LoginViews />
     </>
   );
 };
---
+++ src/views/Auth/Login/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from './Login.module.scss'
+
+const LoginViews = () => {
+    const { push } = useRouter();
+    const handleLogin = () => {
+        push("/product");
+    };
+    return (
+        <div className={styles.login}>
+            <p className="text-3xl font-bold">Login Page</p>
+            <button onClick={handleLogin}>Login</button>
+            <p style={{ color: "red", border: "1px solid red", borderRadius: '10px' }}>
+                Belum punya akun? registrasi <Link href="/auth/register">disini</Link>
+            </p>
+        </div>
+    )
+}
+
+export default LoginViews;
