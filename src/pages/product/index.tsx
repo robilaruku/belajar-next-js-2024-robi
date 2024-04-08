b115c0136c7a818b52e18ca4a46e8f6bcf2c6e6e
Robi Irhamni <robidream20@gmail.com>
2024-04-08 11:35:18 +0700

    add linking and navigating

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -3,17 +3,17 @@ import { useEffect, useState } from "react";
 
 const ProductPage = () => {
     const [isLogin, setIsLogin] = useState(false);
-    const {push} = useRouter();
+    const { push } = useRouter();
     useEffect(() => {
         if (!isLogin) {
-            
+            push("/auth/login");
         }
-    },[])
+    }, []);
     return (
         <>
             <h1>Product Page</h1>
         </>
     );
-}
+};
 
 export default ProductPage;
