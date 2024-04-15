389f82edc21e0d1615bd595a18ec946f9cb71003
Robi Irhamni <robidream20@gmail.com>
2024-04-15 16:14:25 +0700

    minor update env

--- src/views/Product/index.tsx
+++ src/views/Product/index.tsx
@@ -9,41 +9,37 @@ type ProductType = {
 };
 
 const ProductView = ({ products }: { products: ProductType[] }) => {
+  const renderProduct = (product: ProductType) => (
+    <div key={product.id} className={styles.product__content__item}>
+      <div className={styles.product__content__item__image}>
+        <img src={product.image} alt={product.name} />
+      </div>
+      <h4 className={styles.product__content__item__name}>{product.name}</h4>
+      <p className={styles.product__content__item__category}>
+        {product.category}
+      </p>
+      <p className={styles.product__content__item__price}>
+        {product.price.toLocaleString("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        })}
+      </p>
+    </div>
+  );
+
   return (
     <div className={styles.product}>
       <h1 className={styles.product__title}>Product</h1>
       <div className={styles.product__content}>
         {products.length > 0 ? (
-          <>
-            {products.map((product: ProductType) => (
-              <div key={product.id} className={styles.product__content__item}>
-                <div className={styles.product__content__item__image}>
-                  <img src={product.image} alt={product.name} />
-                </div>
-                <h4 className={styles.product__content__item__name}>
-                  {product.name}
-                </h4>
-                <p className={styles.product__content__item__category}>
-                  {product.category}
-                </p>
-                <p className={styles.product__content__item__price}>
-                  {product.price.toLocaleString("id-ID", {
-                    style: "currency",
-                    currency: "IDR",
-                  })}
-                </p>
-              </div>
-            ))}
-          </>
+          products.map(renderProduct)
         ) : (
-          <>
-            <div className={styles.product__content__skeleton}>
-              <div className={styles.product__content__skeleton__image} />
-              <div className={styles.product__content__skeleton__name} />
-              <div className={styles.product__content__skeleton__category} />
-              <div className={styles.product__content__skeleton__price} />
-            </div>
-          </>
+          <div className={styles.product__content__skeleton}>
+            <div className={styles.product__content__skeleton__image} />
+            <div className={styles.product__content__skeleton__name} />
+            <div className={styles.product__content__skeleton__category} />
+            <div className={styles.product__content__skeleton__price} />
+          </div>
         )}
       </div>
     </div>
